14f29d3934cf0db21f20908b1c7050354462f1a3
German Pla <germanps@outlook.es>
2020-07-22 08:09:10 +0200

    change folder docs

---
+++ docs/precache-manifest.1fe005365d3b8b7194a45be30bc5f1c9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "202c9929ad63ff5cec49a0bc700e6aeb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2d5f6890000c1570bc86",
+    "url": "/static/css/main.73d38009.chunk.css"
+  },
+  {
+    "revision": "3ec50c8fead662706b17",
+    "url": "/static/js/2.637c77a8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.637c77a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d5f6890000c1570bc86",
+    "url": "/static/js/main.db0e80f4.chunk.js"
+  },
+  {
+    "revision": "2beaf3c9299542d4240e",
+    "url": "/static/js/runtime-main.c8ba4116.js"
+  }
+]);
